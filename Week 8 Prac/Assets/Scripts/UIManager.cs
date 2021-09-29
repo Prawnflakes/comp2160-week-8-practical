6b3bdf280e9c9176616193db1d979073e1287e63
Prawnflakes <55228585+Prawnflakes@users.noreply.github.com>
2021-09-29 16:49:55 +1000

    Update UIManager.cs
    implemented

--- Week 8 Prac/Assets/Scripts/UIManager.cs
+++ Week 8 Prac/Assets/Scripts/UIManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Singleton
+    static private UIManager instance;
+    static public UIManager Instance
     {
-        
+        get
+        {
+            if (instance == null)
+            {
+                Debug.LogError("There is not UIManager in the scene.");
+            }
+            return instance;
+        }
     }
 
-    // Update is called once per frame
+    public Text scoreText;
+    public string scoreFormat = "Score: {0}";
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+    }
+
     void Update()
     {
-        
+        scoreText.text = string.Format(scoreFormat, Scorekeeper.Instance.Score);
     }
 }
