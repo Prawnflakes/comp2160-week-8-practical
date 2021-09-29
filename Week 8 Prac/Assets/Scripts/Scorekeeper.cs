df0eeb6cf45f6ee84cff4242f86637b454450e9e
Prawnflakes <55228585+Prawnflakes@users.noreply.github.com>
2021-09-29 16:44:55 +1000

    Added scorekeeper and uimanager scripts
    WIP, currently working on scorekeeper

---
+++ Week 8 Prac/Assets/Scripts/Scorekeeper.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Analytics;
+
+public class Scorekeeper : MonoBehaviour
+{
+    // Singleton
+    static private Scorekeeper instance;
+    static public Scorekeeper Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.LogError("There is no Scorekeeper instance in the scene.");
+            }
+            return instance;
+        }
+    }
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+    }
+
+    public int coinScore = 1;
+
+    private int score = 0;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        score = 0;
+    }
+
+    public void GetCoin()
+    {
+        score += scorePerCoin;
+    }
+}
