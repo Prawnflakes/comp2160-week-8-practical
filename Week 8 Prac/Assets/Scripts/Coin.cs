596abcc529746191317d5fa94923c36c05c99441
TwoHundredPlusCollectiveYears <gavin.zhang1@students.mq.edu.au>
2021-09-29 17:44:24 +1000

    Created Player and Coin Scripts
    Created the player and coin scripts and added them to the 'prefab'

---
+++ Week 8 Prac/Assets/Scripts/Coin.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    public LayerMask playerLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        GameObject other = collider.gameObject;
+
+        if(playerLayer.Contains(other))
+        {
+            Destroy(gameObject);
+
+            Scorekeeper.Instance.IncrementScore();
+        }
+    }
+}
---
+++ Week 8 Prac/Assets/Scripts/InputAxes.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputAxes
+{
+    public const string HorizontalMovement = "HorizontalMovement";
+    public const string VerticalMovement = "VerticalMovement";
+}
